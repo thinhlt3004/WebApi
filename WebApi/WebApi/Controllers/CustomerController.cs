bd9388a97e534f129cc69a3dd5296f2c694073ba
Thinh <thinh.lt0496@gmail.com>
2021-12-05 08:53:11 +0700

    Service CRUD

---
+++ WebApi/WebApi/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        Project3Context _ctx;
+
+        public CustomerController(Project3Context ctx)
+        {
+            _ctx = ctx;
+        }
+
+        [HttpGet]
+        public async Task<List<Customer>> GetAll()
+        {
+            return await _ctx.Customers.ToListAsync();
+        }
+    }
+}
---
+++ WebApi/WebApi/Controllers/ServiceController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceController : ControllerBase
+    {
+        Project3Context _ctx;
+
+        public ServiceController(Project3Context ctx)
+        {
+            _ctx = ctx;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Service>>> GetAll()
+        {
+            return Ok(await _ctx.Services.ToListAsync());
+        }
+
+        [HttpGet("{serviceId}")]
+        public async Task<ActionResult<Service>> GetByID(string serviceId)
+        {
+            return Ok(await _ctx.Services.SingleOrDefaultAsync(i => i.Id == serviceId));
+        }
+
+    }
+}
